451cf3bf071a14190b1c780d6b4a168a9cf2b74d
PC\Admin <susmitha.yeruva@gmail.com>
2017-11-02 14:51:36 +0700

    comment box added

--- app/src/main/java/calibrage/payzanagent/fragments/MainFragment.java
+++ app/src/main/java/calibrage/payzanagent/fragments/MainFragment.java
@@ -1,6 +1,8 @@
 package calibrage.payzanagent.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,7 +11,9 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 import calibrage.payzanagent.R;
 import calibrage.payzanagent.activity.HomeActivity;
@@ -58,7 +62,45 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
                 replaceFragment(getActivity(),MAIN_CONTAINER,new AgentRequestsFragment(),TAG,AgentRequestsFragment.TAG);
                 break;
             case R.id.btn_yetToVerify:
-                showToast(getActivity(),"Yet to implement feature");
+                final Dialog adb = new Dialog(getActivity());
+                adb.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                //this is what I did to added the layout to the alert dialog
+                View layout = inflater.inflate(R.layout.row_comment_box, null);
+                adb.setContentView(layout);
+                //  adb.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                adb.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+               /* AlertDialog.Builder alertSeverity = new AlertDialog.Builder(
+                        getActivity(), android.R.style.Theme_Translucent_NoTitleBar);*/
+                // adb.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                //   adb.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                adb.show();
+                final EditText etComment = (EditText) layout.findViewById(R.id.etComment);
+              //  Button btnCancel = (Button) layout.findViewById(R.id.btnCancel);
+                Button btnDone = (Button) layout.findViewById(R.id.btnDone);
+            /*    btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        adb.dismiss();
+                    }
+                });*/
+
+                btnDone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String stComment = etComment.getText().toString();
+                        if (stComment.equalsIgnoreCase("")) {
+                            showToast(getActivity(), "Please write the reason for decline.");
+
+                        } else {
+                            //acceptDecline("Declined", stComment);
+                            adb.dismiss();
+                            //btnCancelbooking.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            //    showToast(getActivity(),"Yet to implement feature");
                 //replaceFragment(getActivity(), MAIN_CONTAINER, new AggrementDocumentsFragment(), TAG, AggrementDocumentsFragment.TAG);
                 break;
             case R.id.btn_approvedAgents:

10863e50c6c754ec823f0bf589f091295f14ec0f
PC\Admin <susmitha.yeruva@gmail.com>
2017-11-02 19:32:47 +0700

    changes of ui

---
+++ app/src/main/java/calibrage/payzanagent/adapter/InProgressRequetAdapter.java
@@ -0,0 +1,113 @@
+package calibrage.payzanagent.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import calibrage.payzanagent.R;
+import calibrage.payzanagent.interfaces.RequestClickListiner;
+import calibrage.payzanagent.model.AgentRequestModel;
+
+/**
+ * Created by Admin on 11/2/2017.
+ */
+
+public class InProgressRequetAdapter extends RecyclerView.Adapter<InProgressRequetAdapter.MyHolder>{
+
+    private Context context;
+    private ArrayList<AgentRequestModel.ListResult> data;
+    private RequestClickListiner requestClickListiner;
+
+    public InProgressRequetAdapter(Context context,ArrayList<AgentRequestModel.ListResult> data ){
+        this.context = context;
+        this.data = data;
+
+    }
+    @Override
+    public InProgressRequetAdapter.MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_inprogress_request, null);
+        InProgressRequetAdapter.MyHolder mh = new InProgressRequetAdapter.MyHolder(v);
+        return mh;
+    }
+
+    @Override
+    public void onBindViewHolder(final InProgressRequetAdapter.MyHolder holder, final int position) {
+        String name = data.get(holder.getAdapterPosition()).getTitleType()+" "+data.get(holder.getAdapterPosition()).getFirstName()+" "+data.get(holder.getAdapterPosition()).getMiddleName()+" "+data.get(holder.getAdapterPosition()).getLastName();
+        String address = data.get(holder.getAdapterPosition()).getAddressLine1()+","+data.get(holder.getAdapterPosition()).getAddressLine2()+","+data.get(holder.getAdapterPosition()).getVillageName()+","+data.get(holder.getAdapterPosition()).getMandalName()+","+data.get(holder.getAdapterPosition()).getDistrictName()+","+data.get(holder.getAdapterPosition()).getProvinceName()+","+data.get(holder.getAdapterPosition()).getCountryName();
+        String mobileNumber = data.get(holder.getAdapterPosition()).getMobileNumber();
+        String email =   data.get(holder.getAdapterPosition()).getEmail();
+
+        if (name == null)
+            holder.tvAgentName.setVisibility(View.GONE);
+        else {
+            holder.tvAgentName.setText(name);
+        }if (mobileNumber== null){
+            holder.tvMobileNumber.setVisibility(View.GONE);
+        }else {
+            holder.tvMobileNumber.setText(data.get(holder.getAdapterPosition()).getMobileNumber());
+        }if (address == null){
+            holder.tvAddress.setVisibility(View.GONE);
+        }else {
+            holder.tvAddress.setText(address);
+        }
+        if (email==null){
+            holder.tvEmail.setVisibility(View.GONE);
+        }else {
+            holder.tvEmail.setText(email);
+        }
+
+
+        //  holder.tvBusinessCategory.setText(data.get(holder.getAdapterPosition()).getAddressLine1());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestClickListiner.onAdapterClickListiner(holder.getAdapterPosition());
+            }
+        }); holder.btnPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestClickListiner.onAdapterClickListiner(holder.getAdapterPosition());
+            }
+        }); holder.btnHold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestClickListiner.onAdapterClickListiner(holder.getAdapterPosition());
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+    public class MyHolder extends RecyclerView.ViewHolder  {
+        TextView tvAgentName,tvBusinessCategory,tvEmail,tvMobileNumber,tvAddress;
+        Button btnPick,btnHold;
+        public MyHolder(View itemView) {
+            super(itemView);
+            tvAgentName=(TextView) itemView.findViewById(R.id.tracking_list_agentname);
+            //    tvBusinessCategory=(TextView)itemView.findViewById(R.id.tracking_list_businesscategory);
+            tvMobileNumber = (TextView)itemView.findViewById(R.id.tracking_list_mobilenumber);
+            tvEmail = (TextView)itemView.findViewById(R.id.tracking_list_email);
+            tvAddress=(TextView)itemView.findViewById(R.id.tracking_list_address);
+            btnPick = (Button)itemView.findViewById(R.id.btn_pick);
+            btnHold = (Button)itemView.findViewById(R.id.btn_hold);
+           /* btnPick.setOnClickListener(this);
+            btnHold.setOnClickListener(this);*/
+        }
+
+
+    }
+    public void setOnAdapterListener(RequestClickListiner onAdapterListener) {
+        this.requestClickListiner = onAdapterListener;
+    }
+}
--- app/src/main/java/calibrage/payzanagent/fragments/InProgressFragment.java
+++ app/src/main/java/calibrage/payzanagent/fragments/InProgressFragment.java
@@ -3,23 +3,18 @@ package calibrage.payzanagent.fragments;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Parcelable;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -30,7 +25,7 @@ import java.util.ArrayList;
 import calibrage.payzanagent.R;
 import calibrage.payzanagent.activity.HomeActivity;
 import calibrage.payzanagent.adapter.AgentRequetAdapter;
-import calibrage.payzanagent.adapter.GenericAdapter;
+import calibrage.payzanagent.adapter.InProgressRequetAdapter;
 import calibrage.payzanagent.interfaces.RequestClickListiner;
 import calibrage.payzanagent.model.AgentRequestModel;
 import calibrage.payzanagent.networkservice.ApiConstants;
@@ -43,10 +38,9 @@ import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class AgentRequestsFragment extends  BaseFragment implements RequestClickListiner {
+public class InProgressFragment extends BaseFragment implements RequestClickListiner {
 
-    public static final String TAG = AgentRequestsFragment.class.getSimpleName();
-
+    public static final String TAG = InProgressFragment.class.getSimpleName();
 
     View view;
     private RecyclerView recyclerView;
@@ -57,29 +51,27 @@ public class AgentRequestsFragment extends  BaseFragment implements RequestClick
     private AgentRequestModel agentRequestModelBundle;
     public  static Toolbar toolbar;
 
-    // implements OnRequestSelected
-    @Nullable
+    public InProgressFragment() {
+        // Required empty public constructor
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_agent_request, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       view = inflater.inflate(R.layout.fragment_in_progress, container, false) ;
         context = this.getActivity();
-        HomeActivity.toolbar.setTitle(getResources().getString(R.string.agentrequest_sname));
+        HomeActivity.toolbar.setTitle(getResources().getString(R.string.inprogressrequest_sname));
         HomeActivity.toolbar.setTitleTextColor(ContextCompat.getColor(context, R.color.white_new));
-        recyclerView = (RecyclerView) view.findViewById(R.id.recylerview_card);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recylerview_card_inprogress);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
         fragmentManager = getActivity().getSupportFragmentManager();
         getRequest(CommonConstants.USERID+"/"+CommonConstants.AGENT_REQUEST_ID);
 
         return view;
-
     }
 
-
-
-
     private void getRequest(String providerType) {
-
         MyServices service = ServiceFactory.createRetrofitService(context, MyServices.class);
         operatorSubscription = service.getRequest(ApiConstants.AGENT_REQUESTS + providerType)
                 .subscribeOn(Schedulers.newThread())
@@ -87,7 +79,7 @@ public class AgentRequestsFragment extends  BaseFragment implements RequestClick
                 .subscribe(new Subscriber<AgentRequestModel>() {
                     @Override
                     public void onCompleted() {
-                       // Toast.makeText(context, "check", Toast.LENGTH_SHORT).show();
+                        // Toast.makeText(context, "check", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -108,11 +100,11 @@ public class AgentRequestsFragment extends  BaseFragment implements RequestClick
 
                     @Override
                     public void onNext(AgentRequestModel agentRequestModel) {
-                       // Log.d("response", agentRequestModel.getIsSuccess().toString());
+                        // Log.d("response", agentRequestModel.getIsSuccess().toString());
                         listResults = (ArrayList<AgentRequestModel.ListResult>) agentRequestModel.getListResult();
-                        AgentRequetAdapter agentRequetAdapter = new AgentRequetAdapter(context, listResults);
-                        recyclerView.setAdapter(agentRequetAdapter);
-                        agentRequetAdapter.setOnAdapterListener(AgentRequestsFragment.this);
+                        InProgressRequetAdapter inProgressRequetAdapter = new InProgressRequetAdapter(context, listResults);
+                        recyclerView.setAdapter(inProgressRequetAdapter);
+                        inProgressRequetAdapter.setOnAdapterListener(InProgressFragment.this);
                         agentRequestModelBundle = agentRequestModel;
 
                     }
@@ -120,7 +112,6 @@ public class AgentRequestsFragment extends  BaseFragment implements RequestClick
                 });
 
     }
-
     public void ReplcaFragment(android.support.v4.app.Fragment fragment) {
         fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
     }
@@ -128,39 +119,15 @@ public class AgentRequestsFragment extends  BaseFragment implements RequestClick
 
     @Override
     public void onAdapterClickListiner(int pos) {
-        final Dialog adb = new Dialog(getActivity());
-        adb.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View layout = inflater.inflate(R.layout.row_comment_box, null);
-        adb.setContentView(layout);
-        adb.getWindow().setLayout(500, 200);
-        adb.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-        adb.show();
-        final EditText etComment = (EditText) layout.findViewById(R.id.etComment);
-        Button btnSubmit = (Button) layout.findViewById(R.id.btnSubmit);
-
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String stComment = etComment.getText().toString();
-                if (stComment.equalsIgnoreCase("")) {
-                    showToast(getActivity(), "Please write the reason for decline.");
-
-                } else {
-                    //acceptDecline("Declined", stComment);
-                    adb.dismiss();
-                    //btnCancelbooking.setVisibility(View.GONE);
-                }
-            }
-        });
-    /*    Bundle bundle = new Bundle();
+             Bundle bundle = new Bundle();
         bundle.putParcelable("request", agentRequestModelBundle);
         bundle.putInt("position",pos);
         Fragment fragment = new RegistrationViewFragment();
         fragment.setArguments(bundle);
 //        ReplcaFragment(fragment);
         replaceFragment(getActivity(), MAIN_CONTAINER, fragment, TAG, RegistrationViewFragment.TAG);
-       // fragmentManager.beginTransaction().replace(R.id.content_frame, fragment,"AgentTag").commit();*/
+       // fragmentManager.beginTransaction().replace(R.id.content_frame, fragment,"AgentTag").commit();
+      //  replaceFragment(getActivity(), MAIN_CONTAINER, new RegistrationViewFragment(), TAG, RegistrationViewFragment.TAG);
+
     }
 }

00e3f41d488af8937e582a8cb264c08b33cfc4c5
PC\Admin <susmitha.yeruva@gmail.com>
2017-11-21 18:39:36 +0700

    added api in personal,bank,id screen susmitha

---
+++ app/src/main/java/calibrage/payzanagent/model/IdProofResponseModel.java
@@ -0,0 +1,182 @@
+package calibrage.payzanagent.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 11/21/2017.
+ */
+
+public class IdProofResponseModel {
+    @SerializedName("Result")
+    @Expose
+    private Result result;
+    @SerializedName("IsSuccess")
+    @Expose
+    private Boolean isSuccess;
+    @SerializedName("AffectedRecords")
+    @Expose
+    private Integer affectedRecords;
+    @SerializedName("EndUserMessage")
+    @Expose
+    private String endUserMessage;
+    @SerializedName("ValidationErrors")
+    @Expose
+    private List<Object> validationErrors = null;
+    @SerializedName("Exception")
+    @Expose
+    private Object exception;
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public Boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+    public void setIsSuccess(Boolean isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+
+    public Integer getAffectedRecords() {
+        return affectedRecords;
+    }
+
+    public void setAffectedRecords(Integer affectedRecords) {
+        this.affectedRecords = affectedRecords;
+    }
+
+    public String getEndUserMessage() {
+        return endUserMessage;
+    }
+
+    public void setEndUserMessage(String endUserMessage) {
+        this.endUserMessage = endUserMessage;
+    }
+
+    public List<Object> getValidationErrors() {
+        return validationErrors;
+    }
+
+    public void setValidationErrors(List<Object> validationErrors) {
+        this.validationErrors = validationErrors;
+    }
+
+    public Object getException() {
+        return exception;
+    }
+
+    public void setException(Object exception) {
+        this.exception = exception;
+    }
+    public class Result {
+
+        @SerializedName("AgentId")
+        @Expose
+        private String agentId;
+        @SerializedName("IdProofTypeId")
+        @Expose
+        private Integer idProofTypeId;
+        @SerializedName("IdProofNumber")
+        @Expose
+        private String idProofNumber;
+        @SerializedName("Id")
+        @Expose
+        private Integer id;
+        @SerializedName("IsActive")
+        @Expose
+        private Boolean isActive;
+        @SerializedName("CreatedBy")
+        @Expose
+        private String createdBy;
+        @SerializedName("ModifiedBy")
+        @Expose
+        private String modifiedBy;
+        @SerializedName("Created")
+        @Expose
+        private String created;
+        @SerializedName("Modified")
+        @Expose
+        private String modified;
+
+        public String getAgentId() {
+            return agentId;
+        }
+
+        public void setAgentId(String agentId) {
+            this.agentId = agentId;
+        }
+
+        public Integer getIdProofTypeId() {
+            return idProofTypeId;
+        }
+
+        public void setIdProofTypeId(Integer idProofTypeId) {
+            this.idProofTypeId = idProofTypeId;
+        }
+
+        public String getIdProofNumber() {
+            return idProofNumber;
+        }
+
+        public void setIdProofNumber(String idProofNumber) {
+            this.idProofNumber = idProofNumber;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public Boolean getIsActive() {
+            return isActive;
+        }
+
+        public void setIsActive(Boolean isActive) {
+            this.isActive = isActive;
+        }
+
+        public String getCreatedBy() {
+            return createdBy;
+        }
+
+        public void setCreatedBy(String createdBy) {
+            this.createdBy = createdBy;
+        }
+
+        public String getModifiedBy() {
+            return modifiedBy;
+        }
+
+        public void setModifiedBy(String modifiedBy) {
+            this.modifiedBy = modifiedBy;
+        }
+
+        public String getCreated() {
+            return created;
+        }
+
+        public void setCreated(String created) {
+            this.created = created;
+        }
+
+        public String getModified() {
+            return modified;
+        }
+
+        public void setModified(String modified) {
+            this.modified = modified;
+        }
+
+    }
+}
